9a4f29da5945eef1ac6cbf5f68a99938a4cf206a
lonefly <76199019+lonefly163@users.noreply.github.com>
2020-12-19 21:08:50 +0800

    update from github.com

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print("New git done")
+    print("edit from github.com")
 
 
 # Press the green button in the gutter to run the script.
